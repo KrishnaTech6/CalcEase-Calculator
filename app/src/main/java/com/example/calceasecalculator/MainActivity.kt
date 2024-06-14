ce61d2279595f3b997459047fb7c4f8af9c244ec
krishnaTech6 <krishna060702@gmail.com>
2024-06-14 11:15:44 +0530

    increased effeciency

--- app/src/main/java/com/example/calceasecalculator/MainActivity.kt
+++ app/src/main/java/com/example/calceasecalculator/MainActivity.kt
@@ -20,23 +20,22 @@ class MainActivity : AppCompatActivity() {
     private var calculationText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 
     fun getData(view: View) {
         vibrate()
-        if(view is Button){
+        if (view is Button) {
             var text = view.text.toString()
-            when(text){
-                "( )" ->{
-                    if (hasOpenBracket){
-                        text = ")"
-                        hasOpenBracket= false
-                    }else{
-                        text = "("
-                        hasOpenBracket= true
+            when (text) {
+                "( )" -> {
+                    text = if (hasOpenBracket) {
+                        hasOpenBracket = false
+                        ")"
+                    } else {
+                        hasOpenBracket = true
+                        "("
                     }
                 }
             }
@@ -45,36 +44,25 @@ class MainActivity : AppCompatActivity() {
             binding.tvShowCalculation.text = calculationText
         }
     }
+
     fun clearField(view: View) {
         vibrate()
-        if (view is Button){
-            //Delete calculation text
+        if (view is Button) {
             calculationText = ""
-            hasOpenBracket= false
+            hasOpenBracket = false
             solvedEquationDisplayed()
             binding.tvShowCalculation.text = calculationText
             binding.tvDisplayAnswer.text = calculationText
         }
-
     }
+
     fun deleteSingle(view: View) {
         vibrate()
-        if (view is Button){
-            if (calculationText.isNotEmpty())
-                calculationText = calculationText.removeRange(calculationText.length-1, calculationText.length)
-            else hasOpenBracket=false
-
-            //For determining if we have to apply open bracket or closed bracket
-            var openBracketCount = 0
-            var closeBracketCount = 0
-            for (char in calculationText){
-                when(char){
-                    '(' -> openBracketCount++
-                    ')' -> closeBracketCount++
-                }
+        if (view is Button) {
+            if (calculationText.isNotEmpty()) {
+                calculationText = calculationText.removeRange(calculationText.length - 1, calculationText.length)
+                hasOpenBracket = checkOpenBrackets()
             }
-            hasOpenBracket =
-                openBracketCount > closeBracketCount //will return true of false , so that next time clicking on brackets button right bracket is shown
             solvedEquationDisplayed()
             binding.tvShowCalculation.text = calculationText
         }
@@ -82,7 +70,7 @@ class MainActivity : AppCompatActivity() {
 
     fun solveEquation(view: View) {
         vibrate()
-        if (view is Button ){
+        if (view is Button) {
             solvedEquationDisplayed(false)
         }
     }
@@ -90,51 +78,53 @@ class MainActivity : AppCompatActivity() {
     private fun evaluateExpression(expression: String): Double {
         return ExpressionBuilder(expression).build().evaluate()
     }
-    private fun solvedEquationDisplayed(hasPreview:Boolean =true){
+
+    private fun solvedEquationDisplayed(hasPreview: Boolean = true) {
         try {
-            val result = evaluateExpression(calculationText.replace("×","*"))
-            if (hasPreview){
-                binding.tvShowCalculation.textSize =44f
-                binding.tvDisplayAnswer.textSize = 30F
-                binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
-                binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.textColor))
-            }else{
-                binding.tvShowCalculation.textSize =30f
-                binding.tvDisplayAnswer.textSize = 44F
-                binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.textColor))
-                binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
-
-            }
-            binding.tvDisplayAnswer.text = result.toString()
-
-        }catch (e:Exception){
-            if (hasPreview){
-                binding.tvShowCalculation.textSize =44f
-                binding.tvDisplayAnswer.textSize = 30F
-                binding.tvDisplayAnswer.text = ""
-                binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
-                binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.textColor))
-            }else{
-                binding.tvShowCalculation.textSize =30f
-                binding.tvDisplayAnswer.textSize = 44F
-                binding.tvDisplayAnswer.text = "Error"
-                binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.textColor))
-                binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
-
+            val result = evaluateExpression(calculationText.replace("×", "*"))
+            updateUIText(hasPreview, result.toString())
+        } catch (e: Exception) {
+            if (hasPreview) {
+                updateUIText(true, "")
+            } else {
+                updateUIText(false, "Error")
             }
         }
     }
 
-    // Function to vibrate the device
+    private fun updateUIText(hasPreview: Boolean, displayText: String) {
+        if (hasPreview) {
+            binding.tvShowCalculation.textSize = 44f
+            binding.tvDisplayAnswer.textSize = 30f
+            binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
+            binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.textColor))
+        } else {
+            binding.tvShowCalculation.textSize = 30f
+            binding.tvDisplayAnswer.textSize = 44f
+            binding.tvDisplayAnswer.setTextColor(ContextCompat.getColor(this, R.color.textColor))
+            binding.tvShowCalculation.setTextColor(ContextCompat.getColor(this, R.color.previewTextColor))
+        }
+        binding.tvDisplayAnswer.text = displayText
+    }
+
+    private fun checkOpenBrackets(): Boolean {
+        var openBracketCount = 0
+        var closeBracketCount = 0
+        for (char in calculationText) {
+            when (char) {
+                '(' -> openBracketCount++
+                ')' -> closeBracketCount++
+            }
+        }
+        return openBracketCount > closeBracketCount
+    }
+
     private fun vibrate() {
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        // Check if the device has vibration capabilities
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
-            // Deprecated in API 26, but still required for devices running older versions
             vibrator.vibrate(100)
         }
     }
-
 }
